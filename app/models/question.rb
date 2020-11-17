976f8943f003d2740974e8a9a5903b837a889686
Alexander Chaschin <chaschin@uchi.ru>
2020-11-18 02:37:13 +0300

    remove question model

--- app/models/question.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Question < ApplicationRecord
-  validates :title, :body, presence: true
-end
--- spec/factories/questions.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :question do
-    title { 'MyString' }
-    body { 'MyText' }
-  end
-end
