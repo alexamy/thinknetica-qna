092c9ea32e94bde060badf9e1dda01b2397d06e6
Alexander Chaschin <chaschin@uchi.ru>
2021-02-17 01:46:54 +0300

    refactor update question test

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -98,8 +98,9 @@ RSpec.describe QuestionsController, type: :controller do
     end
 
     context 'with invalid attributes' do
+      before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+
       it 'does not change question' do
-        patch :update, params: { id: question, question: attributes_for(:question, :invalid) }
         question.reload
 
         expect(question.title).to eq 'MyString'
@@ -107,7 +108,6 @@ RSpec.describe QuestionsController, type: :controller do
       end
 
       it 're-renders edit view' do
-        patch :update, params: { id: question, question: attributes_for(:question, :invalid) }
         expect(response).to render_template :edit
       end
     end
