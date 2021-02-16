d605d32f860843cac50563fd5bad4b05ad158cb0
Alexander Chaschin <chaschin@uchi.ru>
2021-02-17 01:15:02 +0300

    refactor questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,11 @@
 class QuestionsController < ApplicationController
+  before_action :find_question, only: %i[show edit]
+
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def new
@@ -12,6 +13,11 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def find_question
     @question = Question.find(params[:id])
   end
 end
