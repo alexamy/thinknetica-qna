747e4c662d85535807ef017614bfa532f793c92a
Alexander Chaschin <chaschin@uchi.ru>
2021-02-18 02:16:52 +0300

    finish index action

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -6,13 +6,16 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
 
   describe 'GET #index' do
+    let(:answers) { create_list(:answer, 3, question: question) }
+
     before { get :index, params: { question_id: question.id } }
 
     it 'assigns requested question to @question' do
       expect(assigns(:question)).to eq question
     end
 
-    it 'populates array fo all answers'
-    it 'renders index view'
+    it 'renders index view' do
+      expect(response).to render_template(:index)
+    end
   end
 end
