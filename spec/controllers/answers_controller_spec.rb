7e5fb893f846f02eac5371627692d456ccfb7857
Alexander Chaschin <chaschin@uchi.ru>
2021-02-18 02:04:46 +0300

    add index test case

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
 
+  describe 'GET #index' do
+    before { get :index }
+
+    it 'assigns requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'populates array fo all answers'
+    it 'renders index view'
+  end
 end
