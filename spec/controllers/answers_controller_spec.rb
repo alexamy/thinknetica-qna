9b052ff5321940678952ee2c627d5f260b9c110f
Alexander Chaschin <chaschin@uchi.ru>
2021-02-18 12:44:36 +0300

    add new spec

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
 
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns requested question to @question'
+    it 'assigns new answer to @answer'
+    it 'renders new view'
+  end
 end
