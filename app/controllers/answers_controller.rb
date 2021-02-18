78d7e66f52f4843cac4d639992368ed966ec440e
Alexander Chaschin <chaschin@uchi.ru>
2021-02-18 13:21:45 +0300

    fix show test cases

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,8 @@
 class AnswersController < ApplicationController
-  before_action :find_question, only: %i[new create]
+  before_action :find_question, only: %i[new create show]
+  before_action :find_answer, only: %i[show]
+
+  def show; end
 
   def new
     @answer = @question.answers.new
@@ -14,6 +17,10 @@ class AnswersController < ApplicationController
 
   def find_question
     @question = Question.find(params[:question_id])
+  end
+
+  def find_answer
+    @answer = Answer.find(params[:id])
   end
 
   def answer_params
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe AnswersController, type: :controller do
   describe 'GET #show' do
     let(:answer) { create(:answer, question: question) }
 
+    before { get :show, params: { id: answer, question_id: question.id } }
+
     it 'assigns requested question to @question' do
       expect(assigns(:question)).to eq question
     end
