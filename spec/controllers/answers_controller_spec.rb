031c1111a0caa85aed6724e477f04fc65c1238d3
Alexander Chaschin <chaschin@uchi.ru>
2021-02-18 13:16:46 +0300

    add test case

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,9 +4,19 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
 
   describe 'GET #show' do
-    it 'assigns requested question to @question'
-    it 'assigns requested answer to @answer'
-    it 'renders a show view'
+    let(:answer) { create(:answer, question: question) }
+
+    it 'assigns requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns requested answer to @answer' do
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'renders a show view' do
+      expect(response).to render_template(:show)
+    end
   end
 
   describe 'GET #new' do
@@ -31,6 +41,10 @@ RSpec.describe AnswersController, type: :controller do
       it 'saves a new answer to a database' do
         expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) }}
           .to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        expect(response).to render_template(:show)
       end
     end
 
