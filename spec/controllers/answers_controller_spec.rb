67332bdc9cd5a5f6c111305dd783bb56bffbef05
Alexander Chaschin <chaschin@uchi.ru>
2021-02-18 12:48:42 +0300

    add answers test content

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,10 +4,18 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
 
   describe 'GET #new' do
-    before { get :new }
+    before { get :new, params: { question_id: question.id } }
 
-    it 'assigns requested question to @question'
-    it 'assigns new answer to @answer'
-    it 'renders new view'
+    it 'assigns requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns new answer to @answer' do
+      expect(assings(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template(:new)
+    end
   end
 end
