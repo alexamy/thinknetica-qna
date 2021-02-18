ab3bd370ef337d81abde53b160ebf156353a7437
Alexander Chaschin <chaschin@uchi.ru>
2021-02-18 12:51:01 +0300

    add find_question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,11 @@
 class AnswersController < ApplicationController
+  before_action :find_question, only: %i[new]
+
   def new; end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
 end
