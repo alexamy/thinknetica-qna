777f0471e50934db6cc8c94c5e37f7d22cfce6e9
Alexander Chaschin <chaschin@uchi.ru>
2021-02-18 12:06:01 +0300

    This reverts to d4e83d26621ebaf41193a6abcd911de2973260d1

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,11 +1,2 @@
 class AnswersController < ApplicationController
-  before_action :find_question, only: %i[index]
-
-  def index; end
-
-  private
-
-  def find_question
-    @question = Question.find(params[:question_id])
-  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,21 +1,5 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  let(:question) { create(:question) }
 
-  describe 'GET #index' do
-    let(:answers) { create_list(:answer, 3, question: question) }
-
-    before { get :index, params: { question_id: question.id } }
-
-    it 'assigns requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
-
-    it 'renders index view' do
-      expect(response).to render_template(:index)
-    end
-  end
 end
