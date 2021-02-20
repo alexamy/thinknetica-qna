13817daa2edabfc5d8f7c8c8d38d90b4a701a0da
Alexander Chaschin <chaschin@uchi.ru>
2021-02-21 00:16:05 +0300

    add unregistered test case

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -18,5 +18,12 @@ feature 'User can sign in', "
     expect(page).to have_content 'Signed in successfully.'
   end
 
-  scenario 'Unregistered user tries to sign in'
+  scenario 'Unregistered user tries to sign in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password.'
+  end
 end
