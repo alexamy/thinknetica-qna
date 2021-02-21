26d545ab1a0a20a99f73227a9a422e6535731614
Alexander Chaschin <chaschin@uchi.ru>
2021-02-21 23:40:25 +0300

    add view questions spec

---
+++ spec/features/question/view_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can view question list', "
+  In order to find desired question
+  As an user
+  I'd like to be able to see all questions
+" do
+
+  given(:questions) { create_list(:question, 5) }
+
+  scenario 'User views a questions list' do
+    visit questions_path
+
+    expect(page).to have_table('Questions', rows: questions.map(&:title).in_groups_of(1))
+  end
+end
