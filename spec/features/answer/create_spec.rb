8fedbcaf7a977b4b39896a2c23e713865705cc98
Alexander Chaschin <chaschin@uchi.ru>
2021-02-22 11:44:48 +0300

    add authenticated user answer test cases

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -7,9 +7,29 @@ feature 'User can add an answer', "
   As an authenticated user
   I'd like to add an answer for a question
 " do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  background do
+    sign_in(user)
+    visit question_path(question)
+  end
+
   describe 'Authenticated user' do
-    scenario 'adds an answer'
-    scenario 'adds an answer with errors'
+    scenario 'adds an answer' do
+      fill_in 'Answer', with: 'Test answer body'
+      click_on 'Add answer'
+
+      expect(page).to have_content('Your answer was successfully created')
+      expect(page).to have_content('Test answer body')
+    end
+
+    scenario 'adds an answer with errors' do
+      click_on 'Add answer'
+
+      expect(page).to have_content("Body can't be blank")
+    end
   end
 
   scenario 'Unauthenticated user tries to add an answer'
