1eb5e7dfcff78994673dc3b8929520fd6d3f376d
Alexander Chaschin <chaschin@uchi.ru>
2021-02-23 00:46:50 +0300

    add other question delete test case

--- spec/features/question/delete_spec.rb
+++ spec/features/question/delete_spec.rb
@@ -8,21 +8,27 @@ feature 'User can delete question', "
   I'd like to be able to delete my own question
 " do
   given(:user) { create(:user) }
+  given(:other_user) { create(:user) }
+
   given(:question) { create(:question, author: user) }
+  given(:other_question) { create(:question, author: other_user) }
 
   describe 'Authenticated user' do
-    background do
-      sign_in(user)
-      visit question_path(question)
-    end
+    background { sign_in(user) }
 
     scenario 'deletes his question' do
+      visit question_path(question)
       click_on 'Delete'
 
       expect(page).to have_content('Question was successfully deleted.')
     end
 
-    scenario 'tries to delete others question'
+    scenario 'tries to delete others question' do
+      visit question_path(other_question)
+      click_on 'Delete'
+
+      expect(page).to have_content("Can't delete someone else's question")
+    end
   end
 
   scenario 'Unauthenticated user tries to delete question'
