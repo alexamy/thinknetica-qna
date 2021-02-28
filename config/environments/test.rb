c328e24046ea50e1900b7e84ba3f288b02a85512
Alexander Chaschin <chaschin@uchi.ru>
2021-02-28 23:36:02 +0300

    add assets load in test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,6 +46,10 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Load assets
+  config.assets.debug = true
+  config.assets.quiet = true
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 end
