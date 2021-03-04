6dc037c78294e8e448f4aa852158d26890667696
Alexander Chaschin <chaschin@uchi.ru>
2021-03-05 01:32:00 +0300

    test: refactor patch update on questions

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -98,42 +98,32 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  describe 'PATCH #update with valid attributes' do
+  describe 'PATCH #update' do
     before { login(user) }
 
-    it 'assigns requested question to @question' do
-      patch :update, params: { id: question, question: attributes_for(:question) }
-      expect(assigns(:question)).to eq question
+    context 'with valid attributes' do
+      it 'assigns requested question to @question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'changes question attributes' do
+        patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
+        question.reload
+
+        expect(question.title).to eq 'new title'
+        expect(question.body).to eq 'new body'
+      end
     end
 
-    it 'changes question attributes' do
-      patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
-      question.reload
+    context 'with invalid attributes' do
+      it 'does not change question' do
+        patch :update, params: { id: question, question: attributes_for(:question, :invalid) }
+        question.reload
 
-      expect(question.title).to eq 'new title'
-      expect(question.body).to eq 'new body'
-    end
-
-    it 'redirects to updated question' do
-      patch :update, params: { id: question, question: attributes_for(:question) }
-      expect(response).to redirect_to question
-    end
-  end
-
-  describe 'PATCH #update with invalid attributes' do
-    before { login(user) }
-
-    before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
-
-    it 'does not change question' do
-      question.reload
-
-      expect(question.title).to eq 'MyString'
-      expect(question.body).to eq 'MyText'
-    end
-
-    it 're-renders edit view' do
-      expect(response).to render_template :edit
+        expect(question.title).to eq 'MyString'
+        expect(question.body).to eq 'MyText'
+      end
     end
   end
 
