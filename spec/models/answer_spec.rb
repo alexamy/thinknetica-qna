4fd58995b5f02e6b06da3c9b91f5728827d2cb36
Alexander Chaschin <chaschin@uchi.ru>
2021-03-07 20:32:34 +0300

    test: add answer model best_answer association

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Answer, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:question) }
     it { is_expected.to belong_to(:author) }
+    it { is_expected.to have_one(:question_where_is_best) }
   end
 
   describe 'validations' do
