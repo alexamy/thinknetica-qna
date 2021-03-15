f7c981a8309bd06fcf316b220972df3b3d35f4cf
Alexander Chaschin <chaschin@uchi.ru>
2021-03-16 01:24:05 +0300

    chore: move activestorage config line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
 
+  config.active_storage.replace_on_assign_to_many = false
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
@@ -90,8 +92,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.active_storage.replace_on_assign_to_many = false
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
