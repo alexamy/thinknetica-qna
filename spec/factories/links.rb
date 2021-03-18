c95f6ca2627ab5cd8e995a3fcef11f6fce699e8c
Alexander Chaschin <chaschin@uchi.ru>
2021-03-19 02:08:46 +0300

    chore: fix checks warnings

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :link do
-    name { "MyString" }
-    url { "MyString" }
+    name { 'my gist' }
+    url { 'https://gist.github.com/alexamy/259aeed922d066ca09510fd18de20f80' }
   end
 end
